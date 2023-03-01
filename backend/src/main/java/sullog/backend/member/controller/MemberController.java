ab1d95a8fdcd467009245967ce8a41dbd403a358
seungyeonchoi <seungyeonchoi@worksmobile.com>
2023-03-01 22:29:22 +0900

    [refactor] path를 /me로 변경

--- backend/src/main/java/sullog/backend/member/controller/MemberController.java
+++ backend/src/main/java/sullog/backend/member/controller/MemberController.java
@@ -1,21 +1,25 @@
 package sullog.backend.member.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import sullog.backend.member.service.MemberService;
+import sullog.backend.member.service.TokenService;
 
 @RestController
 public class MemberController {
 
+    private final TokenService tokenService;
     private final MemberService memberService;
 
-    public MemberController(MemberService memberService) {
+    public MemberController(TokenService tokenService, MemberService memberService) {
+        this.tokenService = tokenService;
         this.memberService = memberService;
     }
 
-    @DeleteMapping("/members/{memberId}")
-    public void deleteMember(@PathVariable int memberId){
-        memberService.deleteMember(memberId);
+    @DeleteMapping("/members/me")
+    public void deleteMember(@RequestHeader String authorization){
+        String email = tokenService.getEmail(authorization);
+        memberService.deleteMember(email);
     }
 }
