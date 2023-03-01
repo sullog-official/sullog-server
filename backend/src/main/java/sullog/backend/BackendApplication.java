f3333423da27de45e5fe03a1c31de6561ab9d8e6
Jeong-EuiJae <47394504+cnpcnp99@users.noreply.github.com>
2023-03-01 15:15:51 +0900

    feat: 스프링 시큐리티 기능 끄는 설정 추가

--- backend/src/main/java/sullog/backend/BackendApplication.java
+++ backend/src/main/java/sullog/backend/BackendApplication.java
@@ -2,8 +2,9 @@ package sullog.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class BackendApplication {
 
 	public static void main(String[] args) {
