d9cb75aa42c35b2601b8dce57e783846c4f09199
seungyeonchoi <seungyeonchoi@worksmobile.com>
2023-03-04 19:32:10 +0900

    [feat] 사용자 id 기반 경험기록 조회 기능 서비스 로직 추가

--- backend/src/main/java/sullog/backend/record/service/RecordService.java
+++ backend/src/main/java/sullog/backend/record/service/RecordService.java
@@ -1,8 +1,11 @@
 package sullog.backend.record.service;
 
 import org.springframework.stereotype.Service;
+import sullog.backend.record.dto.table.RecordMetaWithAlcoholInfoDto;
 import sullog.backend.record.entity.Record;
 import sullog.backend.record.mapper.RecordMapper;
+
+import java.util.List;
 
 @Service
 public class RecordService {
@@ -15,5 +18,10 @@ public class RecordService {
 
     public void saveRecord(Record record) {
         recordMapper.insertRecord(record);
+    }
+
+    public List<RecordMetaWithAlcoholInfoDto> getRecordMetasByMemberId(int memberId) {
+        List<RecordMetaWithAlcoholInfoDto> recordMetaWithAlcoholInfoDtos = recordMapper.selectRecordMetaByMemberId(memberId);
+        return recordMetaWithAlcoholInfoDtos;
     }
 }
