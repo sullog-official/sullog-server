f4bf833f395a9e3c1fa7da1219052c7080d6e934
seungyeonchoi <seungyeonchoi@worksmobile.com>
2023-05-15 00:15:20 +0900

    feat: memberId로 db에서 회원데이터 조회 실패 시, 예외 처리

--- backend/src/main/java/sullog/backend/common/interceptor/MemberIdInterceptor.java
+++ backend/src/main/java/sullog/backend/common/interceptor/MemberIdInterceptor.java
@@ -4,6 +4,10 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import sullog.backend.auth.service.TokenService;
+import sullog.backend.common.error.ErrorCode;
+import sullog.backend.common.error.exception.BusinessException;
+import sullog.backend.member.entity.Member;
+import sullog.backend.member.service.MemberService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,9 +16,11 @@ import javax.servlet.http.HttpServletResponse;
 public class MemberIdInterceptor implements HandlerInterceptor {
 
     private final TokenService tokenService;
+    private final MemberService memberService;
 
-    public MemberIdInterceptor(TokenService tokenService) {
+    public MemberIdInterceptor(TokenService tokenService, MemberService memberService) {
         this.tokenService = tokenService;
+        this.memberService = memberService;
     }
 
     @Override
@@ -23,7 +29,13 @@ public class MemberIdInterceptor implements HandlerInterceptor {
         if (null == token) { // permitAll로 처리되는 요청은 jwtToken값을 실어보내지 않으므로, 방어로직 추가
             return true;
         }
+
         Integer memberId = tokenService.getMemberId(token);
+        Member findMember = memberService.findMemberById(memberId); // 토큰에 담긴 memberId로 member가 조회되지 않으면 예외 발생
+        if (null == findMember) {
+            throw new BusinessException(ErrorCode.MEMBER_NOT_FOUND);
+        }
+
         request.setAttribute("memberId", memberId);
         return true;
     }
