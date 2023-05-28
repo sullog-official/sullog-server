3d54d6ef4be3c3ec5c954daeaf4aebfc60b89edb
seungyeonchoi <seungyeonchoi@worksmobile.com>
2023-05-28 20:04:50 +0900

    deploy: 배포 자동화 시 pid 얻기 위한 설정 추가

--- backend/src/main/java/sullog/backend/BackendApplication.java
+++ backend/src/main/java/sullog/backend/BackendApplication.java
@@ -2,13 +2,15 @@ package sullog.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication application = new SpringApplication(BackendApplication.class);
+		application.addListeners(new ApplicationPidFileWriter());
+		application.run(args);
 	}
 
 }
