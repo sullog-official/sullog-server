2a8f7cf3c755fbfecb44d6f76cb135610b7b732e
seungyeonchoi <seungyeonchoi@worksmobile.com>
2023-05-28 22:19:39 +0900

    deploy: aws 파라미터 스토어에서 jasypt 암호화키 읽어오도록 수정

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -29,4 +29,4 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 nohup java -jar \
   -DJASYPT_PASSWORD=$JASYPT_PASSWORD \
-  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+  $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
